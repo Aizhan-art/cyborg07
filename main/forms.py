2d409abc7847a528d16c1234b54900abc7522cf3
Aizhan <osrarova.i@icloud.com>
2025-03-06 09:56:40 +0600

    create base.html & modalka

---
+++ main/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import Product
+
+
+class ProductCreateForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = (
+            'title',
+            'category',
+            'main_image',
+            'images',
+            'description',
+            'price',
+            'is_active',
+        )
+
+class ProductUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = (
+            'title',
+            'category',
+            'main_image',
+            'images',
+            'description',
+            'price',
+            'is_active',
+        )
--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, Http404
+from django.contrib import messages
+
+from .forms import ProductCreateForm, ProductUpdateForm
 from .models import Product
 
 
@@ -10,10 +13,32 @@ def index_view(request):
 
 def product_detail_view(request, product_id):
     product = get_object_or_404(Product,id=product_id)
+    product_update_form = ProductUpdateForm()
 
     similar_products = Product.objects.filter(category=product.category).exclude(id=product.id)
 
     return render(
         request=request,
         template_name='main/product_detail.html',
-        context={'product': product, 'similar_products': similar_products})
+        context={'product': product, 'similar_products': similar_products, 'product_update_form': product_update_form})
+
+
+def product_create_view(request):
+    if not request.user.is_authenticated:
+        raise Http404
+
+    if request.method == 'POST':
+        form = ProductCreateForm(request.POST, request.FILES)
+        if form.is_valid():
+            product_object = form.save(commit=False)
+            product_object.user = request.user
+            product_object.save()
+
+            messages.success(request, 'Успешно создано!')
+            return redirect('index')
+
+    form = ProductCreateForm()
+    return render(request, 'main/product_create.html', {'form': form})
+
+#def product_update_view(request, product_id):
+ #   return render(request, 'main/product_update')
